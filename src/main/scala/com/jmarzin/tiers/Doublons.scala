687f28fbfb7110f7eaaf6d3fb2bc4d680f47079c
jmarzin-cp <jacques.marzin@free.fr>
2017-01-01 12:11:42 +0100

    Dimanche 1 janvier 2017

--- src/main/scala/com/jmarzin/tiers/Doublons.scala
+++ src/main/scala/com/jmarzin/tiers/Doublons.scala
@@ -6,10 +6,10 @@ package main.scala.com.jmarzin.tiers
 object Doublons {
 
   def paquetParCode(vect: Vector[Item], dist: Vector[(String,String,Integer)]): Vector[(String,Vector[(String,Integer)])] = {
-    var i = 0
+    AppTiers.nbDebiteurDoublons = 0
     return for(v <- vect) yield {
-      i += 1
-      if (i%10 == 0) println(i)
+      AppTiers.nbDebiteurDoublons += 1
+      if (Thread.interrupted) return Vector(("",Vector(("",0))))
       val resfiltre = dist.filter(p => p._1 == v.code || p._2 == v.code)
       val restrie = resfiltre.sortWith(_._3 < _._3)
       val restronque = restrie.slice(0,10)
@@ -23,11 +23,13 @@ object Doublons {
   }
 
   def cherche: Unit = {
-    print("Chargement des tiers : ")
+    if (Thread.interrupted()) return
     val vecteur = Base.litTiers
-    println(vecteur.vect.length)
+    if (Thread.interrupted()) return
     val res = vecteur.calculDistance
+    if (Thread.interrupted()) return
     val resFinal = paquetParCode(vecteur.vect, res)
+    if (Thread.interrupted()) return
     val resFinalFlat = (for {
       v <- resFinal
       w <- v._2
@@ -40,6 +42,13 @@ object Doublons {
         Triplet(w._1, v._1, w._2)
       }
     }).distinct.sorted
-    AppTiers.finNormale = true
+    if (Thread.interrupted) return
+    Base.sauve(resFinalFlat)
+    if (Thread.interrupted) {
+      return
+    } else {
+      AppTiers.finNormale = true
+      return
+    }
   }
 }
